674054c5a3594765591216e37fff2c15b0ea2547
Azeem <azeemahmed101@gmail.com>
2020-10-15 11:54:11 +0200

    update explorte to appointments page

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -9,7 +9,7 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div className="container">
       <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>Upcoming Appointments <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">Appointment 1</a></p>
     </div>
   );
 };
