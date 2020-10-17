d7d7f253be117484aa4161e14dcbc428f2283bfe
Azeem <azeemahmed101@gmail.com>
2020-10-17 08:33:20 +0200

    update logout reducer state

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,14 @@
-export default function reducer(state={}, action: any): any {
+const initState = {
+  userData: {
+    token: "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.JC6qKuH9SG0SIiYSfhZUFTtirxN9Q47buLk0DPFFFzE",
+    user: {
+      email: "azeem@example.com",
+      username: "azeem"
+    }
+  }
+}
+
+export default function reducer(state=initState, action: any): any {
   switch (action.type) {
     case 'SET_USER':
       return {
@@ -6,9 +16,16 @@ export default function reducer(state={}, action: any): any {
         userData: action.user
       }
     case 'LOG_OUT':
+      return {}
+    case 'SET_APPOINTMENTS':
       return {
         ...state,
-        userData: ''
+        appointments: action.appointments
+      }
+    case 'SET_SERVICES':
+      return {
+        ...state,
+        services: action.services
       }
   }
   return state
