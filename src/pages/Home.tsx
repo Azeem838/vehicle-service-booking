7d449faf159a09795fb53d2ad8e5cd21f9183680
Azeem <azeemahmed101@gmail.com>
2020-10-20 12:22:48 +0200

    style homePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,34 @@
 import { 
-  IonContent,
-  IonHeader,
   IonPage,
-  IonTitle,
-  IonToolbar,
   IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
 } from '@ionic/react';
 import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home Page</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonButton routerLink="/login">Login</IonButton>
-        <IonButton routerLink="/register" color="secondary">Register</IonButton>
-      </IonContent>
+    <IonPage style={{background: "#96bf00"}}>
+      <IonGrid>
+        <IonRow className="ion-align-items-center" style={{height: "100%"}}>
+          <IonCol>
+            <div className="ion-padding">
+              <div className="ion-text-center">
+                <IonText color="muted" style={{color: "white"}}>
+                    <h2>Welcome</h2>
+                    <h3>Book a service today!</h3>
+                    <hr style={{borderWidth: "1px"}}/>
+                    <p>We offer a range of service from, quick to custom engine rebuilds - Book now to avoid disappointment</p>
+                </IonText>
+                <IonButton color="light" fill="outline" routerLink="/login">Login</IonButton>
+                <IonButton color="light" routerLink="/register">Register</IonButton>
+              </div>
+            </div>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
     </IonPage>
   );
   
