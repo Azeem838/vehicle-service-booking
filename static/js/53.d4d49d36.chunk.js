eaab175cf1237b8f4b871708e616fb69d0fb9c5e
Azeem <azeemahmed101@gmail.com>
2020-10-21 21:17:48 +0200

    Updates

---
+++ static/js/53.d4d49d36.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvehicle-service-booking"]=this["webpackJsonpvehicle-service-booking"]||[]).push([[53],{164:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(8),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.j)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.g)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.d4d49d36.chunk.js.map
